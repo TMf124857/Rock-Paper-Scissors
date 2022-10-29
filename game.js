5a55061b6f269029c43bf2d2adca7720540c730f
tmf124857 <chooseadmas@gmail.com>
2022-10-29 13:03:32 -0400

    Createde getComputerChoice function in game.js and tested succecfuly console.log

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
 console.log("hello World")
+
+// Creating a function that return at random rock, paper or scissor
+function getComputerChoice () {
+    const play = ["Rock", "Paper", "Scissors"];
+    let show = play[Math.floor(Math.random() * play.length)];
+    return show
+}
