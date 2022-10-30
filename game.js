bca0116a97eced24ef49b647af964811fe22017e
tmf124857 <chooseadmas@gmail.com>
2022-10-30 04:45:53 -0400

    Wrote a function that play a single round that take two parameters playerSelection and computerSelection, then return a string of the result

--- game.js
+++ game.js
@@ -6,3 +6,24 @@ function getComputerChoice () {
     let show = play[Math.floor(Math.random() * play.length)];
     return show
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            return result;
+        } else {
+            result = `You Win! '${playerSelection} beats ${computerSelection}.`;
+            return result;  
+        }
+    }
+        
+}
+
+  
+   
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
